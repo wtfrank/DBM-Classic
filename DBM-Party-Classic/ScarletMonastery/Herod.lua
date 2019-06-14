052bbe7a129eb22cb3465c27b549bca392cf5a52
MysticalOS <mysticalosx@gmail.com>
2019-06-14 03:34:30 -0400

    Herod spellId updates, but events may still be wrong

--- DBM-Party-Classic/ScarletMonastery/Herod.lua
+++ DBM-Party-Classic/ScarletMonastery/Herod.lua
@@ -8,21 +8,30 @@ mod:SetCreatureID(3975)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 13736"
+	"SPELL_CAST_START 8989",
+	"SPELL_AURA_APPLIED 8269"
 )
 
-local specWarnWhirlwind				= mod:NewSpecialWarningRun(13736, nil, nil, nil, 4, 2)
+--TODO: Is WW SPELL_CAST_START or success or applied?
+local specWarnWhirlwind				= mod:NewSpecialWarningRun(8989, nil, nil, nil, 4, 2)
+local warningEnrage					= mod:NewTargetNoFilterAnnounce(8269, 2)
 
-local timerWhirlwindCD				= mod:NewAITimer(180, 13736, nil, nil, nil, 4, nil, DBM_CORE_DEADLY_ICON)
+local timerWhirlwindCD				= mod:NewAITimer(180, 8989, nil, nil, nil, 4, nil, DBM_CORE_DEADLY_ICON)
 
 function mod:OnCombatStart(delay)
 	timerWhirlwindCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 8040 then
+	if args.spellId == 8989 then
 		specWarnWhirlwind:Show()
 		specWarnWhirlwind:Play("justrun")
 		timerWhirlwindCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 8269 then
+		warningEnrage:Show(args.destName)
 	end
 end
