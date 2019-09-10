d1ebc7627d47230fe89b21be4b1579dc150e4287
MysticalOS <mysticalosx@gmail.com>
2019-09-10 15:13:00 -0400

    Fixed version check for classic BW

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -416,7 +416,7 @@ local delayedFunction
 local dataBroker
 local voiceSessionDisabled = false
 
-local fakeBWVersion, fakeBWHash = 147, "b9e4c1e"
+local fakeBWVersion, fakeBWHash = 3, "9ab6c0c"
 local versionQueryString, versionResponseString = "Q^%d^%s", "V^%d^%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
