cef61f14c24dd41394c115c86aaf06391ebbb935
wallrik <wallrik@users.noreply.github.com>
2019-10-22 21:00:15 +0200

    Basic boss tracking for UBRS
    Basic combat detection with English locales and Conflagration alert for General Drakkisath

---
+++ DBM-Party-Classic/UpperBlackrockSpire/WarchiefRendBlackhand.lua
@@ -0,0 +1,7 @@
+local mod	= DBM:NewMod("WarchiefRendBlackhand", "DBM-Party-Classic", 4)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision("@file-date-integer@")
+mod:SetCreatureID(10429)
+
+mod:RegisterCombat("combat")
