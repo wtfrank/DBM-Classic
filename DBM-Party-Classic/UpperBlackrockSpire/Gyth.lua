5e75db39d210cbffff6d56fae66ecad8badaa3e5
wallrik <wallrik@users.noreply.github.com>
2019-10-24 19:32:59 +0200

    Remove Gyth as separate boss and include in Rend

--- DBM-Party-Classic/UpperBlackrockSpire/Gyth.lua
+++
@@ -1,7 +0,0 @@
-local mod	= DBM:NewMod("Gyth", "DBM-Party-Classic", 4)
-local L		= mod:GetLocalizedStrings()
-
-mod:SetRevision("@file-date-integer@")
-mod:SetCreatureID(10339)
-
-mod:RegisterCombat("combat")
--- DBM-Party-Classic/UpperBlackrockSpire/WarchiefRendBlackhand.lua
+++ DBM-Party-Classic/UpperBlackrockSpire/WarchiefRendBlackhand.lua
@@ -2,6 +2,7 @@ local mod	= DBM:NewMod("WarchiefRendBlackhand", "DBM-Party-Classic", 4)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
-mod:SetCreatureID(10429)
+mod:SetCreatureID(10339, 10429) -- Gyth, Rend
+mod:SetMainBossID(10429)
 
 mod:RegisterCombat("combat")
