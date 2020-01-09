2ad57b93b8148fddc1986986734427b474cdf9a1
MysticalOS <mysticalosx@gmail.com>
2020-01-09 14:24:36 -0500

    Drycode target scanning mind control on Lucifron to warn during cast instead of cast finish

--- DBM-MC/Lucifron.lua
+++ DBM-MC/Lucifron.lua
@@ -8,8 +8,9 @@ mod:SetModelID(13031)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 19702 19703",
-	"SPELL_AURA_APPLIED 20604"
+	"SPELL_CAST_START 20604",
+	"SPELL_CAST_SUCCESS 19702 19703"
+--	"SPELL_AURA_APPLIED 20604"
 )
 
 --[[
@@ -19,6 +20,9 @@ local warnDoom		= mod:NewSpellAnnounce(19702, 2)
 local warnCurse		= mod:NewSpellAnnounce(19703, 3)
 local warnMC		= mod:NewTargetNoFilterAnnounce(20604, 4)
 
+local specWarnMC	= mod:NewSpecialWarningYou(20604, nil, nil, nil, 1, 2)
+local yellMC		= mod:NewYell(20604)
+
 local timerCurseCD	= mod:NewCDTimer(20.5, 19703, nil, nil, nil, 3, nil, DBM_CORE_CURSE_ICON)--20-25N)
 local timerDoomCD	= mod:NewCDTimer(20, 19702, nil, nil, nil, 3, nil, DBM_CORE_MAGIC_ICON)--20-25
 --local timerDoom		= mod:NewCastTimer(10, 19702, nil, nil, nil, 3, nil, DBM_CORE_MAGIC_ICON)
@@ -26,6 +30,33 @@ local timerDoomCD	= mod:NewCDTimer(20, 19702, nil, nil, nil, 3, nil, DBM_CORE_MA
 function mod:OnCombatStart(delay)
 	timerDoomCD:Start(7-delay)--7-8
 	timerCurseCD:Start(12-delay)--12-15
+end
+
+do
+	local MindControl = DBM:GetSpellInfo(20604)
+	function mod:MCTarget(targetname, uId)
+		if not targetname then return end
+		warnMC:CombinedShow(1, targetname)
+		if targetname == UnitName("player") then
+			specWarnMC:Show()
+			specWarnMC:Play("targetyou")
+			yellMC:Yell()
+		end
+	end
+
+	function mod:SPELL_CAST_START(args)
+		local spellName = args.spellName
+		if spellName == MindControl and args:IsSrcTypeHostile() then
+			self:BossTargetScanner(args.sourceGUID, "MCTarget", 0.2, 8)
+		end
+	end
+
+	--[[function mod:SPELL_AURA_APPLIED(args)
+		--if args.spellId == 20604 then
+		if args.spellName == MindControl then
+			warnMC:CombinedShow(1, args.destName)
+		end
+	end--]]
 end
 
 do
@@ -48,16 +79,6 @@ do
 				warnCurse:Show()
 				timerCurseCD:Start()
 			end
-		end
-	end
-end
-
-do
-	local MindControl = DBM:GetSpellInfo(20604)
-	function mod:SPELL_AURA_APPLIED(args)
-		--if args.spellId == 20604 then
-		if args.spellName == MindControl then
-			warnMC:CombinedShow(1, args.destName)
 		end
 	end
 end
