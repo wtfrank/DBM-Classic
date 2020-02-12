8fa5e2fc8361d22ec2fad5cf5678f84d003bc89d
MysticalOS <mysticalosx@gmail.com>
2020-02-12 09:14:01 -0500

    Update version check ahead of BWL

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -424,7 +424,7 @@ local dataBroker
 local voiceSessionDisabled = false
 local handleSync
 
-local fakeBWVersion, fakeBWHash = 5, "793f905"
+local fakeBWVersion, fakeBWHash = 9, "4bfc8af"
 local versionQueryString, versionResponseString = "Q^%d^%s", "V^%d^%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
