210a5a23429f2aba06b16b6fe59dd0e9ff8b6578
MysticalOS <mysticalosx@gmail.com>
2020-02-14 18:48:41 -0500

    Bump classic version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -70,8 +70,8 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@project-date-integer@"),
-	DisplayVersion = "1.13.34 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2020, 2, 10) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DisplayVersion = "1.13.34", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2020, 2, 14) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
